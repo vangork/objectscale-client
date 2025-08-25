2a1152370c78b0bdbd31f59693224a7fd42ee9d0
Luis Liu <luis_liu@dell.com>
2025-08-25 21:20:10 +0800

    Add provisioning tests
    Signed-off-by: Luis Liu <luis_liu@dell.com>

--- tests/provisioning_tests.rs
+++ tests/provisioning_tests.rs
@@ -2,57 +2,58 @@ mod common;
 use objectscale_client::provisioning::{BucketBuilder, BucketTag};
 use objectscale_client::tenancy::NamespaceBuilder;
 
+const REPLICATION_GROUP: &str =
+    "urn:storageos:ReplicationGroupInfo:0e953ad1-94a5-4eb1-825a-d58d29e85434:global";
+
 #[test]
 fn test_bucket() {
     let mut management_client = common::create_management_client();
 
-    let namespace_name = "test_bucket";
+    let namespace_name = "provisioning_test_bucket";
     let namespace = NamespaceBuilder::default()
         .name(namespace_name)
+        .default_data_services_vpool(REPLICATION_GROUP)
         .build()
-        .expect("build namespace");
+        .expect("new namespace");
     let namespace = management_client
         .create_namespace(namespace)
         .expect("create namespace");
 
-    let bucket_name = "testbucket";
-    let expiration = -1;
+    let bucket_name = "provisioning_test_bucket";
     let bucket = BucketBuilder::default()
         .name(bucket_name)
         .namespace(&namespace.id)
-        .audit_delete_expiration(expiration)
-        .tags(vec![BucketTag {
-            key: "key1".to_string(),
-            value: "value1".to_string(),
-        }])
         .build()
-        .expect("build bucket");
-    let _ = management_client
+        .expect("new bucket");
+    let mut bucket = management_client
         .create_bucket(bucket)
         .expect("create bucket");
+    assert_eq!(bucket.name, bucket_name);
+    assert_eq!(bucket.namespace, namespace.id);
+    assert_eq!(bucket.tags.len(), 0);
 
-    let mut bucket = management_client
+    let tags = vec![BucketTag {
+        key: "key1".to_string(),
+        value: "value1".to_string(),
+    }];
+    bucket.tags = tags.clone();
+
+    let state = management_client
+        .update_bucket(bucket)
+        .expect("update bucket");
+    assert_eq!(state, true);
+
+    let bucket = management_client
         .get_bucket(&bucket_name, &namespace.id)
         .expect("get bucket");
     assert_eq!(bucket.name, bucket_name);
     assert_eq!(bucket.namespace, namespace.id);
-    assert_eq!(bucket.audit_delete_expiration, expiration);
-    assert_eq!(bucket.tags.len(), 1);
-
-    let new_expiration = 0;
-    bucket.audit_delete_expiration = new_expiration;
-    let bucket = management_client
-        .update_bucket(bucket)
-        .expect("update bucket");
-    assert_eq!(bucket.audit_delete_expiration, new_expiration);
-    assert_eq!(bucket.name, bucket_name);
-    assert_eq!(bucket.namespace, namespace.id);
-    assert_eq!(bucket.tags.len(), 1);
+    assert_eq!(bucket.tags, tags);
 
     let buckets = management_client
         .list_buckets(&namespace.id, "")
         .expect("list buckets");
-    assert_ne!(buckets.len(), 0);
+    assert!(buckets.contains(&bucket));
 
     management_client
         .delete_bucket(&bucket_name, &namespace.id, false)
